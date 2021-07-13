5cfda63474300941bb09fefe26bd4fe34b035309
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2021-07-13 08:30:43 -0300

    Modificações cenário level 5

---
+++ Assets/scripts/spike.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spike : MonoBehaviour
+{
+    public float speed;
+    public float moveTime;
+
+    private bool cima = true;
+    private float timer;
+
+    void Update()
+    {
+        if(cima) 
+        {
+           transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        }
+        else 
+        {
+          transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        }
+        timer += Time.deltaTime;
+
+        if(timer>= moveTime)
+        {
+            cima = !cima;
+            timer = 0f;
+        }
+
+    }
+ }
