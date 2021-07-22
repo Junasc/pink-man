9e98e264f482cad4e95437b3645d588689348dcf
Juliana <pessoal.juliana.n@gmail.com>
2021-07-22 10:22:50 -0300

    Adiconado boss ultima fase

--- Assets/scripts/Boss.cs
+++ Assets/scripts/Boss.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-   
+    public float radious;
+    public float speed;
+    public float moveTime;
+    public bool direita = true;
+    public bool cima = true;
+    private float timer;
+
+
+    void Update()
+    {
+        if(direita)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        }
+        timer += Time.deltaTime;
+        if(timer>= moveTime)
+        {
+            direita = !direita;
+            timer = 0f;
+        }
+
+     if(cima)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        timer += Time.deltaTime;
+        if(timer>= moveTime)
+        {
+            cima = !cima;
+            timer = 3f;
+        }
+    }
 }
+
+
+
+
